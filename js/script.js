51dba08dfcd44a9b70c6d1239330b8ce090b5596
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-05-27 17:46:23 -0400

    feat: Load NYT articles regarding the queried city

--- js/script.js
+++ js/script.js
@@ -24,6 +24,22 @@ function loadData() {
     console.log(src);
     $body.append('<img class="bgimg" src="' + src +'">');
 
+    var nytURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?' +
+                 'q='+ city + '&sort=newest&api-key=' + nytKey;
+    $.getJSON(nytURL, function (data) {
+      $nytHeaderElem.text('New York Times Articles about ' + city);
+
+      $.each(data.response.docs, function (index, val) {
+        var $link = $('<a>').attr('href', val.web_url)
+        $link.text(val.headline.main);
+        var $paragraph = $('<p>').text(val.snippet);
+        var $li = $('<li>').addClass('article');
+        $nytElem.append($li.append($link).append($paragraph));
+      });
+    })
+    .fail(function() {
+      $nytHeaderElem.text('New York Times Articles could not be loaded');
+    });
 
     return false;
 };
