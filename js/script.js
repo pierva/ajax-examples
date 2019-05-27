b5e543f2ff3814eef5d3048bec76fc14d3dad714
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-05-27 17:11:02 -0400

    feat: Update background through Google Street View API

--- js/script.js
+++ js/script.js
@@ -14,6 +14,16 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var location = street + ', ' + city;
+    $greeting.text('So, you want to live at ' + location + '?');
+    var src = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&' +
+              'location=' + location +
+              '&key=' + gStreetViewKey + '';
+    console.log(src);
+    $body.append('<img class="bgimg" src="' + src +'">');
+
 
     return false;
 };
